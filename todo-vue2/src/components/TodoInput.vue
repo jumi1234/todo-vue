d1b65da687d34f241e9b708d9bbed87d294edec4
jumi1234 <farewellsad89@gmail.com>
2021-01-11 17:46:07 +0900

    Todo Input 입력값 localstorage에 저장하기

--- todo-vue2/src/components/TodoInput.vue
+++ todo-vue2/src/components/TodoInput.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="add">
-    <input type="text" class="add-input" placeholder="할 일을 입력하세요" v-model="newTodoItem" />
-    <button class="add-button">추가</button>
+    <input
+      type="text"
+      class="add-input"
+      placeholder="할 일을 입력하세요"
+      v-model="newTodoItem"
+      v-on:keyup.enter="addTodoItem" />
+    <button class="add-button" v-on:click="addTodoItem">
+      <span class="blind">추가</span>
+    </button>
   </div>
 </template>
 
@@ -10,6 +17,21 @@
     data() {
       return {
         newTodoItem: ""
+      }
+    },
+    methods: {
+      addTodoItem() {
+        if(this.newTodoItem !== "") {
+          var value = {
+            item: this.newTodoItem,
+            date: `${new Date().getMonth() + 1}${new Date().getDate()}`
+          }
+          localStorage.setItem(this.newTodoItem, JSON.stringify(value))
+          this.clearInput()
+        }
+      },
+      clearInput() {
+        this.newTodoItem = ""
       }
     }
   }
